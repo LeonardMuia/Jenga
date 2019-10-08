1888d1234394dec5528fb138775b491d533cb028
Leonard Muia <leonardlouis25@outlook.com>
2019-10-08 20:14:25 +0300

    latest update

--- app/src/main/java/com/example/android/jengaapp/auth.java
+++ app/src/main/java/com/example/android/jengaapp/auth.java
@@ -2,6 +2,7 @@ package com.example.android.jengaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.android.jengaapp.ui.auth.AuthFragment;
@@ -12,6 +13,7 @@ public class auth extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
 
 
     }
