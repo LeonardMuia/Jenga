1c3be26455c48c6247af028d667094fbb902b085
Leonard Muia <leonardlouis25@outlook.com>
2019-10-09 04:42:28 +0300

    latest update

--- app/src/main/java/com/example/android/jengaapp/Intro.java
+++ app/src/main/java/com/example/android/jengaapp/Intro.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class Intro extends AppCompatActivity {
-    public static int SPLASH_TIME_OUT = 4000;
+    public static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/android/jengaapp/auth.java
+++ app/src/main/java/com/example/android/jengaapp/auth.java
@@ -2,11 +2,21 @@ package com.example.android.jengaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class auth extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.customerRegister)
+    public void registerView(){
+        setContentView(R.layout.register);
+    }
+
 }
