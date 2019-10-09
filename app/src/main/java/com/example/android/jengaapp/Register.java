57bdd111f1b637f4904b90164cdd62ee926f9f14
Leonard Muia <leonardlouis25@outlook.com>
2019-10-09 05:26:11 +0300

    latest update

--- app/src/main/java/com/example/android/jengaapp/Register.java
+++ app/src/main/java/com/example/android/jengaapp/Register.java
@@ -1,16 +1,22 @@
 package com.example.android.jengaapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
         ButterKnife.bind(this);
+        loginView();
+    }
+
+    @OnClick(R.id.loginText)
+    public void loginView(){
+        setContentView(R.layout.login);
     }
 }
